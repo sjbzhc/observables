981a7e390d93b98244a5cf1534e6ef264cd96528
Manuel ROJAS SOTO <mr.manuell@gmail.com>
2018-07-01 16:04:02 +0200

    Added Subject

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
-import 'rxjs/Rx';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Observer, interval, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    // const myNumbers = interval(1000);
-    // myNumbers.subscribe(
-    //   (number: Number) => {
-    //     console.log(number);
-    //   }
-    // );
+    const myNumbers = interval(1000).pipe(
+      map(
+        (data: number) => {
+          return data * 2;
+        }
+      )
+    )
+    this.numbersObsSubscription = myNumbers.subscribe(
+      (number: Number) => {
+        console.log(number);
+      }
+    );
     const myObservable = Observable.create((observer: Observer<string>) => {
       setTimeout(() => {
         observer.next('first package');
@@ -26,13 +34,22 @@ export class HomeComponent implements OnInit {
         observer.next('second package');
       },4000);
       setTimeout(() => {
-        observer.error('error package');
+        observer.complete()
+        // observer.error('error package');
       },5000);
+      setTimeout(() => {
+        observer.next('third package');
+      },6000);
     });
 
-    myObservable.subscribe(
+    this.customObsSubscription = myObservable.subscribe(
       (data: String) => {console.log(data); },
       (error: String) => {console.log(error); }),
       () => {console.log('Completed'); }
   };
+
+  ngOnDestroy() {
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
+  }
 }
