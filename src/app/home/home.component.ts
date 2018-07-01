19a5c81699ec2863dbb77cc741aa203867cbb9ba
Manuel ROJAS SOTO <mr.manuell@gmail.com>
2018-07-01 15:33:44 +0200

    Added observable manually

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,27 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+    // const myNumbers = interval(1000);
+    // myNumbers.subscribe(
+    //   (number: Number) => {
+    //     console.log(number);
+    //   }
+    // );
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      },2000);
+      setTimeout(() => {
+        observer.next('second package');
+      },4000);
+      setTimeout(() => {
+        observer.error('error package');
+      },5000);
+    });
 
+    myObservable.subscribe(
+      (data: String) => {console.log(data); },
+      (error: String) => {console.log(error); }),
+      () => {console.log('Completed'); }
+  };
 }
